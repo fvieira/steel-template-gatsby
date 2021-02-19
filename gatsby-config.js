3707f09c338a8d08480ac0c466c5ec2e7cd47b83
Francisco Vieira <francisco@moscadigital.pt>
2021-02-19 10:27:58 +0000

    Configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,10 @@ module.exports = {
   siteMetadata: {
     title: `steel-template-gatsby`,
     //must be the url your frontend is deployed to!!!
-    siteUrl: '',
-    description: 'Steel is a sudo framework for headless ecommerce utilizing Snipcart & Sanity ',
-    author: 'stordahl'
+    siteUrl: "https://steel-studio-gatsby-86mbreiz.netlify.app/",
+    description:
+      "Steel is a sudo framework for headless ecommerce utilizing Snipcart & Sanity ",
+    author: "stordahl",
   },
   plugins: [
     `gatsby-plugin-fontawesome-css`,
@@ -12,11 +13,12 @@ module.exports = {
       resolve: `gatsby-plugin-styled-components`,
     },
     {
-      resolve: 'gatsby-plugin-snipcartv3',
+      resolve: "gatsby-plugin-snipcartv3",
       options: {
-        apiKey: '',
+        apiKey:
+          "YjNmNDQ3MTYtYjRlZC00ZWEzLWIzNzUtZDBhMjU0MDE5MTU5NjM3NDkzMjY3NDQ1MzEzOTAx",
         autopop: true,
-      }
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -33,15 +35,16 @@ module.exports = {
     {
       resolve: `gatsby-source-sanity`,
       options: {
-        projectId: ``,
+        projectId: `igl5lycm`,
         dataset: `production`,
         // a token with read permissions is required
         // if you have a private dataset
-        token: '',
+        token:
+          "skZKsr8wVmEATcMEVWfg5xUzZVPOw50oByJDtDH9Xp6L79SjbiLeyFVtI8UKDK7nm1SQWffGRKYQNGm04z7Z1i2CCMlMmqyiV2ahQnrhVx3cnMSt2KXuZbCY7BkECJhCnPafjCb2JQjDTiVjnMWfK2QrRmvpsJC6VaX3LIX6mqtOkBmOk0T5",
 
         // If the Sanity GraphQL API was deployed using `--tag <name>`,
         // use `graphqlTag` to specify the tag name. Defaults to `default`.
-        graphqlTag: 'default',
+        graphqlTag: "default",
       },
     },
   ],
